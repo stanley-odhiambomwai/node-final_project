dcba2cfdb0c1edd217e2b1d15fd41b8e723d96ee
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2024-12-01 13:29:13 +0300

    creating the server and package.json file

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Hello Node!!!!</h1>\n");
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
